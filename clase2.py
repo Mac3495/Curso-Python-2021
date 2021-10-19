46468ae76854f2136283150739724aa6ca871625
Marcelo Torrez <elmac395@gmail.com>
2021-10-18 20:07:09 -0400

    Clase 2

---
+++ clase2.py
@@ -0,0 +1,42 @@
+#ESTA ES LA FUNCION PRINT
+print('SANDWICH')
+
+#ESTO ES UN SANDWICH
+print ('PAN')
+print ('TOMATE')
+print ('CARNE')
+print ('LECHUGA')
+print ('PAN')
+
+#VARIABLES
+variable1 = 5
+print (variable1)
+
+variable1 = 20
+print (variable1)
+
+variable2 = 'Palabra 1'
+variable3 = False
+print (variable3)
+
+#TIPOS DE DATOS
+#NUMERICOS, PALABRAS(STRINGS), BOOLEANOS -> VERDAD(True) || FALSO(False)
+
+#Numeros Enteros, Decimal, Imaginario
+numero1 = 10
+numero2 = -10
+
+#Numeros Decimales(Float)
+decimal1 = 1.85
+decimal2 = 5.456
+decimal3 = 5,2
+print (decimal3)
+#Numeros imaginarios
+imaginario1 = 1j
+
+variable4, variable5 = 5 , "Hola"
+
+print(variable4, variable5)
+
+print (type(decimal3))
+
