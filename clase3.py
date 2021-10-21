bc7822a07c94b2be36901eaa1795df2f123a1543
Marcelo Torrez <elmac395@gmail.com>
2021-10-20 20:04:59 -0400

    Clase 3

---
+++ clase3.py
@@ -0,0 +1,69 @@
+numero1 = 5
+numero2 = 5
+
+suma = numero1 + numero2
+
+print("La suma es: ", suma)
+
+numero1 = 10
+cadena1 = "Hola "
+suma = numero1 + numero2
+
+print(cadena1 + "La suma es: ", suma)
+
+resta = numero1 - numero2
+print("La resta es: ", resta)
+
+#2x5 -> *
+producto = numero1 * numero2
+print("El producto es: ", producto)
+
+division1 = numero1 / numero2
+print("La division es: ", division1)
+
+division2 = numero1 // numero2
+print("La division exacta es: ", division2)
+
+division3 = numero1 % numero2
+print("Los decimales son: ", division3)
+
+# 10 / 5
+#  0   2
+
+exp = numero1 ** numero2
+print("El exponente es: ", exp)
+
+#Strings
+
+cadena2 = "Hola "
+cadena3 = "Mundo"
+
+cadena4 = cadena2 * 5
+print(cadena4)
+
+cadena5 = "Hola a tod@s mi nombre es Marcelo"
+print(len(cadena5))
+
+#Sub Strings
+sub1 = cadena5[7:12]
+print(sub1)
+
+print(cadena5.find('nombres'))
+
+print(cadena5.upper())
+
+cadena6 = 'ESTA ES UNA CADENA EN MAYUSCULA'
+print(cadena6.lower())
+
+print("Hola\nMundo\t2021")
+
+#Booleanos -> True || False
+ivan = "Hola"
+juanita = "Mundo"
+
+print(ivan == juanita)
+print(ivan != juanita)
+print(ivan <= juanita)
+print(ivan >= juanita)
+
+#IF, FOR, WHILE
